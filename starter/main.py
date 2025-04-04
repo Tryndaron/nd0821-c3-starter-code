fefc836ca70de35d5dba0876cd0b9500080d0f5b
Tryndaron <aaron.roggenland@gmx.net>
2025-04-04 09:50:58 +0200

    parths fixed

--- starter/main.py
+++ starter/main.py
@@ -6,9 +6,9 @@ from starter.ml.data import process_data
 from starter.train_model import cat_features
 from joblib import load
 
-lr_model = load('nd0821-c3-starter-code/starter/model/lr_model.joblib')
-encoder = load('nd0821-c3-starter-code/starter/model/encoder_path.joblib')
-lb = load('nd0821-c3-starter-code/starter/model/lb_path.joblib')
+lr_model = load('starter/model/lr_model.joblib')
+encoder = load('starter/model/encoder_path.joblib')
+lb = load('starter/model/lb_path.joblib')
 
 
 class Census_Data(BaseModel):
