605914c4d3a6f2cc7f784efc8348b2c8719e0e0f
Tryndaron <aaron.roggenland@gmx.net>
2025-04-07 16:18:58 +0200

    added

--- starter/tests/test_main.py
+++ starter/tests/test_main.py
@@ -38,23 +38,23 @@ def test_lower_50k():
 
 
 def test_higher_50k():
-    person = {""
-    'age': 43,
-    'workclass': 'Self-emp-not-inc',
-    'fnlgt': '292175',
-    'education': 'Masters',
-    'education_num': 14 ,
-    'marital-status': 'Divorced',
-    'occupation': 'Exec-managerial',
-    'relationsship': 'Unmarried',
-    'race': 'White',
-    'sex': 'Female',
-    'capital-gain': 0,
-    'capital-loss': 0,
-    'hours-per-week': 45,
-    'native-country': 'United-States'
-    ""
-    }
+   person = {""
+  "age": 0,
+  "workclass": "string",
+  "fnlgt": 0,
+  "education": "string",
+  "education-num": 0,
+  "marital-status": "string",
+  "occupation": "string",
+  "relationship": "string",
+  "race": "string",
+  "sex": "string",
+  "capital-gain": 0,
+  "capital-loss": 0,
+  "hours-per-week": 0,
+  "native-country": "string"
+  ""
+}
     resp = client.post("/predict", json=person)
     assert resp.status_code == 200
     assert resp.json() == {"predictions":[">50K"] }
