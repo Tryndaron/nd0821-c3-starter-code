11717a40f208c540e110e2b72b6e2320430a5eca
Tryndaron <aaron.roggenland@gmx.net>
2025-04-07 11:46:29 +0200

    path

--- starter/main.py
+++ starter/main.py
@@ -62,7 +62,7 @@ app = FastAPI()
 
 # Load model and label binarizer
 model_path = "starter/model/lr_model.pkl"
-lb_path = "/nd0821-c3-starter-code/starter/model/lb_path.pkl"
+lb_path = "starter/model/lb_path.pkl"
 
 with open(model_path, "rb") as model_file:
     lr_model = pickle.load(model_file)
@@ -70,7 +70,7 @@ with open(model_path, "rb") as model_file:
 with open(lb_path, "rb") as lb_file:
     lb = pickle.load(lb_file)
 
-with open("/nd0821-c3-starter-code/starter/model/encoder_path.pkl", "rb") as encoder_file:
+with open("starter/model/encoder_path.pkl", "rb") as encoder_file:
     encoder = pickle.load(encoder_file)
 
 
